bd79dedd1f0d828c6784833d9c9b462b89aa6def
root <root@localhost.localdomain>
2019-09-12 19:12:41 +0200

    view_travel_passengers has been added successfully

---
+++ view_travel_passengers.php
@@ -0,0 +1,129 @@
+<?php
+  include("header_driver.php");
+?>
+
+<style>
+
+  table td {
+    font-size: 1.5rem;
+    font-weight: 300;
+}
+
+.table {
+    width: 90%;
+    max-width: 100%;
+    margin-bottom: 20px;
+    margin-left: 70px;
+    margin-top: 0px;
+    margin-right: 90px;
+}
+
+table th {
+    font-size: 1.7rem;
+    font-weight: 400;
+    background: #424c59;
+    color: #ffffff;
+}
+</style>
+
+<div class="container">
+    <!-- <h1>Travel all around Rwanda</h1> -->
+    <h1 id=""> List of Registered Passengers </h1> 
+</div>
+
+<table class="table">
+  <thead class="thead-dark">
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Name</th>
+      <th scope="col">Email</th>
+      <th scope="col">Phone Number</th>
+      <th scope="col">Address</th>
+      <th scope="col">Gender</th>
+      <th scope="col">Age</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Larry</td>
+      <td>the Bird</td>
+      <td>@twitter</td>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+    <tr>
+      <th scope="row">4</th>
+      <td>Larry</td>
+      <td>the Bird</td>
+      <td>@twitter</td>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+    <tr>
+      <th scope="row">5</th>
+      <td>Larry</td>
+      <td>the Bird</td>
+      <td>@twitter</td>
+      <td></td>
+      <td></td>
+      <td></td>
+
+    </tr>
+    <tr>
+      <th scope="row">6</th>
+      <td>Larry</td>
+      <td>the Bird</td>
+      <td>@twitter</td>
+      <td></td>
+      <td></td>
+      <td></td>
+
+    </tr>
+    <tr>
+      <th scope="row">7</th>
+      <td>Larry</td>
+      <td>the Bird</td>
+      <td>@twitter</td>
+      <td></td>
+      <td></td>
+      <td></td>
+
+    </tr>
+    <tr>
+      <th scope="row">8</th>
+      <td>Larry</td>
+      <td>the Bird</td>
+      <td>@twitter</td>
+      <td></td>
+      <td></td>
+      <td></td>
+
+    </tr>
+  </tbody>
+</table>
+
+<br><br><br><br><br><br><br><br><br><br><br>
+<?php
+  include("footer.php");
+?>
